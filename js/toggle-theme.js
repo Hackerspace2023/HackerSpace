be4bf7cf7d24fedb15bb868d5aa24a637f98ec32
nitinryali <rnssrinivas1@gmail.com>
2023-10-10 23:39:18 +0530

    Toggle theme redesigned

--- js/toggle-theme.js
+++ js/toggle-theme.js
@@ -1,4 +1,9 @@
-var theme=document.getElementById("change-theme");
-theme.onclick=function(){
-    document.body.classList.toggle("light-theme");
-}
+// var theme=document.getElementById("change-theme");
+// theme.onclick=function(){
+//     document.body.classList.toggle("light-theme");
+// }
+
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("light-theme")
+})
